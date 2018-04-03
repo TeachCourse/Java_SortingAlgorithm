84ca17655965ba357fa1513fd6f60e1b93319345
Administrator <123456>
2018-04-03 10:25:41 +0800

    init

--- src/cn/teachcourse/SortOfBubble.java
+++ src/cn/teachcourse/SortOfBubble.java
@@ -12,6 +12,14 @@ public class SortOfBubble {
 		// int num[] = { 4, 6, 2, 5, 8, 9 };
 		int num[] = { 6, 1, 2, 5, 4, 3, 9, 7, 10, 8 };
 
+		designAlgorithmB(num);
+
+		for (int i = 0; i < num.length; i++) {
+			System.out.println("num[" + i + "]=" + num[i]);
+		}
+	}
+
+	private static void designAlgorithmA(int[] num) {
 		for (int i = 0; i < num.length - 1; i++) {
 			int tmp;// ÁÙÊ±±äÁ¿
 			for (int j = 0; j < num.length - 1; j++) {
@@ -22,10 +30,19 @@ public class SortOfBubble {
 				}
 			}
 		}
-
-		for (int i = 0; i < num.length; i++) {
-			System.out.println("num[" + i + "]=" + num[i]);
-		}
 	}
 
+	public static void designAlgorithmB(int[] a) {
+		for (int i = a.length - 1; i >= 0; i--) {
+			for (int j = a.length - 1; j >= 0; j--) {
+				int temp;
+				
+				if (j - 1 >= 0 && a[j-1] > a[j]) {
+					temp=a[j-1];
+                    a[j-1]=a[j];
+                    a[j]=temp;
+				}
+			}
+		}
+	}
 }
--- src/cn/teachcourse/SortOfSelector.java
+++ src/cn/teachcourse/SortOfSelector.java
@@ -11,6 +11,20 @@ public class SortOfSelector {
 	public static void main(String[] args) {
 		int num[] = { 3, 2, 5, 1, 8, 0, 6 };
 
+		designAlgorithmB(num);
+
+		for (int p = 0; p < num.length; p++) {
+			System.out.println("num[" + p + "]=" + num[p]);
+		}
+
+	}
+    /**
+     * 常规的选择排序算法：查找最小元素的数组下标
+     * 1、其他元素与选择的元素比较，记录最小元素的数组下标，
+     * 2、并将最小元素作为新选择的元素
+     * @param num
+     */
+	public static void designAlgorithmA(int[] num) {
 		for (int i = 0; i < num.length - 1; i++) {
 			int temp = num[i];// 临时变量
 			int low = i;// 记录数组最小元素下标
@@ -19,16 +33,23 @@ public class SortOfSelector {
 					low = j;
 				}
 			}
-			// 将最小元素移动到最左边
-
+			// 将最小元素移动到左边i的位置
 			num[i] = num[low];
 			num[low] = temp;
 		}
+	}
 
-		for (int p = 0; p < num.length; p++) {
-			System.out.println("num[" + p + "]=" + num[p]);
+	public static void designAlgorithmB(int[] a) {
+		for (int i = a.length - 1; i >= 0; i--) {
+			int temp = a[i];
+			int large = i;
+			for (int j = i - 1; j >= 0; j--) {
+				if (a[large] < a[j])
+					large = j;
+			}
+			a[i] = a[large];
+			a[large] = temp;
 		}
-
 	}
 
 }
